9e74413aa97f92256e6702d2569c3e18d24fe99f
MP <vinimadd@gmail.com>
2020-04-16 18:16:46 +0200

    upadte project, change a bit fetching, try my best

--- app.js
+++ app.js
@@ -1,283 +1,101 @@
-const cardBodyCave = document.getElementById("cave");
-const cave = document.createElement("class", "container");
-cardBodyCave.appendChild(cave);
+//get an element from existing html, create a new element that will append to the html el with new content
 
-const cardBodyForest = document.getElementById("forest");
-const forest = document.createElement("class", "container");
-cardBodyForest.appendChild(forest);
+const cardBody = document.getElementById("main");
+const container = document.createElement("class", "container");
 
-const cardBodyGrassland = document.getElementById("grass");
-const grass = document.createElement("class", "container");
-cardBodyGrassland.appendChild(grass);
+cardBody.appendChild(container);
 
-const cardBodyMountain = document.getElementById("mountain");
-const mountain = document.createElement("class", "container");
-cardBodyMountain.appendChild(mountain);
+//trying to dynamically get the url and pagination prev/next => not perfect, some if statements needed
 
-const cardBodyRare = document.getElementById("rare");
-const rare = document.createElement("class", "container");
-cardBodyRare.appendChild(rare);
+pokemon("https://pokeapi.co/api/v2/pokemon");
 
-const cardBodyRough = document.getElementById("rough");
-const rough = document.createElement("class", "container");
-cardBodyRough.appendChild(rough);
-
-const cardBodySea = document.getElementById("sea");
-const sea = document.createElement("class", "container");
-cardBodySea.appendChild(sea);
-
-const cardBodyUrban = document.getElementById("urban");
-const urban = document.createElement("class", "container");
-cardBodyUrban.appendChild(urban);
-
-const cardBodyWater = document.getElementById("water");
-const water = document.createElement("class", "container");
-cardBodyWater.appendChild(water);
-
-function caveGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/1")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-     
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      cave.appendChild(card);
-      card.appendChild(h1);      
-
-    });
-  });
-}
-caveGroup();
-
-// function forestGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/2")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      forest.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function grasslandGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/3")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      grass.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function mountainGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/4")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      mountain.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function rareGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/5")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      rare.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function rough-terrainGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/6")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      rough.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function seaGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/7")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      sea.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function urbanGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/8")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      urban.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-// function water-edgeGroup(){
-fetch("https://pokeapi.co/api/v2/pokemon-habitat/9")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    let poketest = data.pokemon_species;
-    poketest.forEach(element => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = element.name;
-
-      let pokeurl = element.url;
-      getPokeData(pokeurl, card);
-
-      water.appendChild(card);
-      card.appendChild(h1);
-    });
-  });
-
-  function getPokeData (pokeurl, container) {
-    let url = pokeurl; // <--- this is saving the pokemon url to a variable to use in the fetch. 
-                          //Example: https://pokeapi.co/api/v2/pokemon/1/"
-    fetch(url)
+function pokemon(url) {
+  fetch(url)
     .then(response => response.json())
-    .then(function(pokeData){
-      let pokeColor = pokeData.color.name;
-      let pokeCapture= pokeData.capture_rate;
-      let pokeGrowth = pokeData.growth_rate.name;
-      let pokeShape = pokeData.shape.name;
-      let pokeID = pokeData.id;
+    .then(data => {
+      let allPoke = data.results;
 
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+      allPoke.forEach(element => {
+        pokeurl = element.url;
+        getPokeData(pokeurl);
+      });
 
-        const capture = document.createElement("p");
-        capture.textContent = "Capture rate: " + pokeCapture;
-        const color = document.createElement("p");
-        color.textContent = "Pokemon color: " + pokeColor;
-        const growth = document.createElement("p");
-        growth.textContent = "Pokemon growth rate: " + pokeGrowth;
-        const shape = document.createElement("p");
-        shape.textContent = "Pokemon shape: " + pokeShape;
+      document.getElementById("next").addEventListener("click", () => {
+        pokemon(data.next);
+        next.setAttribute('button', 'btn');
+      });
 
-        container.appendChild(card);
-        
-        createPokeImage(pokeID, card);
-        card.append(capture, color, growth, shape);
-        
-      
+      document.getElementById("previous").addEventListener("click", () => {
+        pokemon(data.previous);
+        previous.setAttribute('button', 'btn');
+      });
+
     })
-  }
-
-  function createPokeImage(pokeID, containerDiv){
-    let pokeImgContainer = document.createElement('div')
-    pokeImgContainer.classList.add('image')
-
-    let pokeImage = document.createElement('img')
-    pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`
-
-    pokeImgContainer.append(pokeImage);
-    containerDiv.append(pokeImgContainer);
+    .catch(err => {
+      console.log(err);
+    });
 }
+
+//from given url fetch the desired data
+
+function getPokeData(pokeurl) {
+  let url = pokeurl;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(pokeData => {
+      appendAll(pokeData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+//pick the data and pic you want to get and append to a card then append this card to a created element appened to html el
+//a small issue here => small cards don't want to separate, probably there is a problem with appending card(s) to the caontainer
+//unfortunately couldn't find the solution
+
+function appendAll(pokeData) {
+  let pokeID = pokeData.id;
+  let pokeAbility = pokeData.abilities;
+  let ability = [];
+
+  pokeAbility.forEach(a => {
+    ability += a.ability.name + ", ";
+  });
+
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  const name = document.createElement("h1");
+  name.textContent = pokeData.name;
+  const height = document.createElement("h3");
+  height.textContent = "Height: " + pokeData.height;
+  const weight = document.createElement("h3");
+  weight.textContent = "Weight: " + pokeData.weight;
+  const experience = document.createElement("h3");
+  experience.textContent = "Base experience: " + pokeData.base_experience;
+  const abilities = document.createElement("h3");
+  abilities.textContent = "Abilities: " + ability;
+
+  card.appendChild(name);
+  createPokeImage(pokeID, card);
+  card.append(abilities, height, weight, experience);
+  container.appendChild(card);
+}
+//get pic using pokemon id from a webside (when there's no hope, internet is my best friend)
+
+function createPokeImage(pokeID, containerDiv) {
+  let pokeImgContainer = document.createElement("div");
+  pokeImgContainer.classList.add("image");
+
+  let pokeImage = document.createElement("img");
+  pokeImage.srcset = `https://pokeres.bastionbot.org/images/pokemon/${pokeID}.png`;
+
+  pokeImgContainer.append(pokeImage);
+  containerDiv.append(pokeImgContainer);
+}
+
+//and I know I'm missing a filter, I'd use array.find()  or array.filter() search bar and I know that with body-parser I can get the condition
 
 //   https://pokeapi.co/api/v2/pokemon?limit=964
